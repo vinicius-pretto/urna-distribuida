ae750daaf52b68d6899f582b3f596fc9a2287654
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-12-03 15:54:41 -0200

    Adding administrador

---
+++ administrador/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.DISTRIBUIDOR_PORT || 3006,
+    comissarioURL: process.env.COMISSARIO_URL || 'http://localhost:3000'
+}
---
+++ administrador/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+require('./index.routes')(app);
+
+module.exports = app;
