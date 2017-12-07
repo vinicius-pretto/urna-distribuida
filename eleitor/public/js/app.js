94eaff72d0a3c0d2f8a8b1e52041a03d2a82952a
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-12-06 23:49:06 -0200

    Adding ui-router

--- eleitor/public/js/app.js
+++ eleitor/public/js/app.js
@@ -2,6 +2,22 @@
   'use strict';
 
   angular.module('app', [
-    
-  ]);
+    'ui.router'
+  ])
+  .config(function($stateProvider) {
+    var loginState = {
+      name: 'login',
+      url: '/',
+      component: 'login'
+    }
+  
+    var votacaoState = {
+      name: 'votacao',
+      url: '/votacao',
+      templateUrl: '/js/votacao/votacao.html'
+    }
+  
+    $stateProvider.state(loginState);
+    $stateProvider.state(votacaoState);
+  });
 })();
---
+++ eleitor/public/js/login/login.component.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .component('login', {
+      templateUrl:'/js/login/login.html',
+      controller: LoginController,
+      controllerAs: 'vm',
+      bindings: {
+        
+      },
+    });
+
+  LoginController.$inject = ['LoginService', '$state'];
+
+  function LoginController(LoginService, $state) {
+    var vm = this;
+    vm.login = login;
+
+    function login(voter) {
+      LoginService.login(voter)
+        .then(function() {
+          $state.go('votacao');
+        })
+        .catch(function(error) {
+          console.error('Unauthorized');
+        });
+    }
+
+    vm.$onInit = function() { };
+    vm.$onChanges = function(changesObj) { };
+    vm.$onDestroy = function() { };
+  }
+})();
