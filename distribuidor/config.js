d62528a1d2dac5e4d272689b130b52f924579441
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-12-06 23:09:55 -0200

    Adding cors

--- distribuidor/config.js
+++ distribuidor/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.DISTRIBUIDOR_PORT || 3002,
-    comissarioURL: process.env.COMISSARIO_URL || 'http://localhost:3000'
+    comissarioURL: process.env.COMISSARIO_URL || 'http://localhost:3000',
+    eleitorURL: process.env.ELEITOR_URL || 'http://localhost:3003'
 }
--- distribuidor/src/app.js
+++ distribuidor/src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ELEITOR_URL = require('../config').eleitorURL;
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', ELEITOR_URL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 require('./index.routes')(app);
 
