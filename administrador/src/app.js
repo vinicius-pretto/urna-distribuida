a003ccdf4796bc5567d7ade26686adcafdcdebf8
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-12-07 01:30:27 -0200

    Adding cors

--- administrador/src/app.js
+++ administrador/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const ELEITOR_URL = require('../config').eleitorURL;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', ELEITOR_URL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 require('./index.routes')(app);
   
